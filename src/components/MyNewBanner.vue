e9a61661e4930d97043fd1ec578b6c03855f2ebb
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-19 15:54:06 +0100

    Fatto

--- src/components/MyNewBanner.vue
+++ src/components/MyNewBanner.vue
@@ -2,6 +2,7 @@
 
     <section class="banner_wrapper">
         <h1>{{bannerTitle}}</h1>
+        <p>{{bannerText}}</p>
     </section>
 
 </template>
@@ -11,7 +12,8 @@ export default {
     name: 'MyNewBanner',
     data: function(){
         return {
-            bannerTitle: 'La Casa de Papel'
+            bannerTitle: 'La Casa de Papel',
+            bannerText: 'La casa di carta narra di un gruppo di rapinatori composto da nove persone. L’obiettivo è una super rapina alla zecca di stato spagnola a Madrid, la Fabrica Nacional de Moneda y Timbre. L’ideatore del colpo è un uomo chiamato il Professore. Ha selezionato altri 8 componenti a cui viene dato il nome di una città: Tokyo, Mosca, Denver, Berlino, Nairobi, Rio, Helsinki ed Oslo.'
         };
     }
 }
@@ -21,6 +23,9 @@ export default {
     .banner_wrapper{
         width: 100%;
         height: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
         background-image: url('../assets/img/la-casa-di-carta-jumbo.jpg');
         background-position: center;
         background-size: cover;
@@ -33,5 +38,13 @@ export default {
         letter-spacing: 2rem;
         text-indent: 2rem;
         font-weight: bold;
+        align-self: center;
+    }
+    p{  
+        align-self: flex-end;
+        font-size: 20px;
+        max-width: 50%;
+        color: red;
+        text-align: center;
     }
 </style>
